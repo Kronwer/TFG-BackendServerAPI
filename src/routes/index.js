0074d2e8329be4e66ad9671d0c00d8c7943cb538
Christian <christiangarcia11598@gmail.com>
2022-08-23 21:28:34 +0200

    Comments added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
 const router = Router();
+
+// Import coordinates controller
 const { getCoordinates, createCoordinate } = require('../controllers/coordinates.controller');
+// Import buildings controller
 const { getBuildings, updateBuilding, createBuilding, deleteBuilding } = require('../controllers/building.controller');
 
+// Coordinates
 router.get('/coordinates', getCoordinates);
 router.post('/coordinates', createCoordinate);
 
+// Buildings
 router.get('/buildings', getBuildings);
 router.post('/buildings', createBuilding);
 router.put('/buildings/:id', updateBuilding);
