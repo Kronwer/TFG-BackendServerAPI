85fad35edfc7398b6b8f636c6b009abd226eb03b
Christian <christiangarcia11598@gmail.com>
2022-08-23 20:16:25 +0200

    Last changes

--- src/controllers/coordinates.controller.js
+++ src/controllers/coordinates.controller.js
@@ -19,49 +19,33 @@ const getCoordinates = async (req, res) => {
     const building = req.query.building;
     const startDate = req.query.startDate;
     const endDate = req.query.endDate;
-    const response = await pool.query('SELECT * FROM coordinate WHERE timestamp >= $1 AND timestamp < $2', [startDate, endDate]);
+    const response = await pool.query('SELECT * FROM coordinate WHERE building = $1 AND timestamp >= $2 AND timestamp < $3', [building, startDate, endDate]);
     res.status(200).json(response.rows);
 }
 
-const getCoordinateById = async (req, res) => {
-    const id = req.params.id;
-    const response = await pool.query('SELECT * FROM coordinate WHERE id = $1', [id]);
-    res.json(response.rows.at(0));
-}
-
 const createCoordinate = async (req, res) => {
-    const { building, latitude, longitude, timestamp } = req.body;
-    const response = await pool.query('INSERT INTO coordinate (building, latitude, longitude, timestamp) VALUES ($1, $2, $3, $4)', [building, latitude, longitude, timestamp]);
-    res.json({
-        message: 'Coordinate Added Succesfully',
-        body: {
-            coordinate: { building, latitude, longitude, timestamp }
+    const { building, latitude, longitude, timestamp, floorNumber } = req.body;
+    if ((latitude >= -90 &&  latitude <= 90) && (longitude >= -180 && longitude <= 180)) {
+        try {
+            await pool.query('INSERT INTO coordinate (building, latitude, longitude, timestamp, floorNumber) VALUES ($1, $2, $3, $4, $5)', [building, latitude, longitude, timestamp, floorNumber]);
+            res.json({
+                message: 'Coordinate Added Succesfully',
+                body: {
+                    coordinate: { building, latitude, longitude, timestamp, floorNumber }
+            }
+        })
+        } catch(err) {
+            res.status(500).send(`Couldn't create coordinate, please check the attributes are correct`);
+            console.error('Error while creating a new coordinate');
         }
-    })
-}
-
-const updateCoordinate = async (req, res) => {
-    const id = req.params.id;
-    const { latitude, longitude } = req.body;
-    const response = await pool.query('UPDATE coordinate SET latitude = $1, longitude = $2 WHERE id = $3',[
-        latitude,
-        longitude,
-        id
-    ]);
-    res.send('Coordinate updated successfully');
-}
-
-const deleteCoordinate = async (req, res) => {
-    const id = req.params.id;
-    const response = await pool.query('DELETE FROM coordinate WHERE id = $1', [id]);
-    console.log(response);
-    res.json(`Coordinate ${id} deleted successfully`);
+    } else {
+        res.status(400).send('Invalid values for latitude and longitude, please check');
+        console.error('Invalid range of values for latitude and longitude');
+    }
+    
 }
 
 module.exports = {
     getCoordinates,
-    getCoordinateById,
-    createCoordinate,
-    updateCoordinate,
-    deleteCoordinate
+    createCoordinate
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,10 @@
 const { Router } = require('express');
 const router = Router();
-const { getCoordinates, getCoordinateById,
-        createCoordinate, updateCoordinate,
-        deleteCoordinate } = require('../controllers/coordinates.controller');
+const { getCoordinates, createCoordinate } = require('../controllers/coordinates.controller');
 const { getBuildings, updateBuilding, createBuilding, deleteBuilding } = require('../controllers/building.controller');
 
 router.get('/coordinates', getCoordinates);
-router.get('/coordinates/:id', getCoordinateById);
 router.post('/coordinates', createCoordinate);
-router.put('/coordinates/:id', updateCoordinate);
-router.delete('/coordinates/:id', deleteCoordinate);
 
 router.get('/buildings', getBuildings);
 router.post('/buildings', createBuilding);
