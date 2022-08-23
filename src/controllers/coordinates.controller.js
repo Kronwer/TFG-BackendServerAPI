2aad04ac2790cfb4dee289c75a761e5184e29a36
Christian <christiangarcia11598@gmail.com>
2022-08-23 20:26:12 +0200

    .env variables added

--- src/controllers/coordinates.controller.js
+++ src/controllers/coordinates.controller.js
@@ -8,11 +8,11 @@ var types = pg.types;
 });
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'admin',
-    database: 'tfg',
-    port: '5432'
+    host: process.env.BD_HOST,
+    user: process.env.BD_USER,
+    password: process.env.BD_PASSWORD,
+    database: process.env.BD_DATABASE,
+    port: process.env.BD_PORT
 });
 
 const getCoordinates = async (req, res) => {
